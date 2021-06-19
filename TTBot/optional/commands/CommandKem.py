bbf8d368b6ba2c74af9c2b8c77b8405b25d18456
axelalex2 <axelalex2@gmx.de>
2021-06-20 01:49:29 +0200

    #21 - simplified and boosted CommandKem

--- TTBot/optional/commands/CommandKem.py
+++ TTBot/optional/commands/CommandKem.py
@@ -1,8 +1,11 @@
+# vendor
+import minidi
+
 # local
 from .Command import Command
+from TTBot.logic.InputSanitizer import InputSanitizer
 
 class CommandKem(Command):
-    
     @staticmethod
     def getCommandString() -> str:
         return 'kem'
@@ -11,15 +14,12 @@ class CommandKem(Command):
     def getRightsId() -> str:
         return 'kem'
 
-    async def execute(self, args) -> str:
-        reply = 'kem1W'
-        try:
-            number = int(args[0])
-            if number > 10:
-                number = 10
-            while (number > 1):
-                number = number - 1
-                reply = reply + ' kem1W'
-        except:
-            reply = 'kem1W'
-        return reply
+    async def execute(self, args: list) -> str:
+        pInputSanitizer: InputSanitizer = minidi.get(InputSanitizer)
+
+        count = args[0] if args and pInputSanitizer.isInteger(args[0]) else 1
+        count = min(max(1, count), 10) # force 1 <= count <= 10
+        listCount = ['kem1W'] * count
+        return ' '.join(listCount)
+    # async def execute(self, args: list) -> str
+# class CommandKem(Command)
